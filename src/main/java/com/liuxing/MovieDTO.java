2514588a272368dc6f6637bcb5bc22e5b7a2fe4e
OdyWangJIAN <w1665120704@outlook.com>
2018-04-10 19:20:06 +0800

    王健新增的

--- src/main/java/com/liuxing/MovieDTO.java
+++
@@ -1,7 +0,0 @@
-package com.liuxing;
-
-/**
- * Created by W1665 on 2018/4/10.
- */
-public class MovieDTO {
-}
--- src/main/java/com/liuxing/Movieone.java
+++
@@ -1,7 +0,0 @@
-package com.liuxing;
-
-/**
- * Created by Administrator on 2018/4/10.
- */
-public class Movieone {
-}
