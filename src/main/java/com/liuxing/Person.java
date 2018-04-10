1e0dd1b15e61009f958dc841ba9971daebeb93b7
invoker <592723124@qq.com>
2018-04-10 17:30:56 +0800

    first create project

---
+++ src/main/java/com/liuxing/Person.java
@@ -0,0 +1,16 @@
+package com.liuxing;
+
+/**
+ * Created by Administrator on 2018/4/10.
+ */
+public class Person {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
