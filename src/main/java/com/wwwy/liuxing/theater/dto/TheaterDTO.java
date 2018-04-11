a48a17459c248458a5687ad884f4ca15937a733c
OdyWangJIAN <w1665120704@outlook.com>
2018-04-11 09:58:26 +0800

    王健新增的area_mapper.xml,city_mapper.xml,hallmovie_mapper.xml,movie_mapper.xml,theater_mapper.xml等mapper映射文件

--- src/main/java/com/wwwy/liuxing/theater/dto/TheaterDTO.java
+++ src/main/java/com/wwwy/liuxing/theater/dto/TheaterDTO.java
@@ -32,6 +32,122 @@ public class TheaterDTO implements Serializable {
     private String theaterPhone;
 
     /**
-     * 电影院
+     * 电影院地图经度
      */
+    private String theaterLongitude;
+
+    /**
+     * 电影院地图纬度
+     */
+    private String theaterLatitude;
+
+    public TheaterDTO() {
+    }
+
+    public TheaterDTO(Integer theaterId, String theaterName, String theaterAddress, String theaterPhone, String theaterLongitude, String theaterLatitude) {
+        this.theaterId = theaterId;
+        this.theaterName = theaterName;
+        this.theaterAddress = theaterAddress;
+        this.theaterPhone = theaterPhone;
+        this.theaterLongitude = theaterLongitude;
+        this.theaterLatitude = theaterLatitude;
+    }
+
+    @Override
+    public String toString() {
+        return "TheaterDTO{" +
+                "theaterId=" + theaterId +
+                ", theaterName='" + theaterName + '\'' +
+                ", theaterAddress='" + theaterAddress + '\'' +
+                ", theaterPhone='" + theaterPhone + '\'' +
+                ", theaterLongitude='" + theaterLongitude + '\'' +
+                ", theaterLatitude='" + theaterLatitude + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        TheaterDTO that = (TheaterDTO) o;
+
+        if (theaterId != null ? !theaterId.equals(that.theaterId) : that.theaterId != null) {
+            return false;
+        }
+        if (theaterName != null ? !theaterName.equals(that.theaterName) : that.theaterName != null) {
+            return false;
+        }
+        if (theaterAddress != null ? !theaterAddress.equals(that.theaterAddress) : that.theaterAddress != null) {
+            return false;
+        }
+        if (theaterPhone != null ? !theaterPhone.equals(that.theaterPhone) : that.theaterPhone != null) {
+            return false;
+        }
+        return theaterLongitude != null ? theaterLongitude.equals(that.theaterLongitude) : that.theaterLongitude == null && (theaterLatitude != null ? theaterLatitude.equals(that.theaterLatitude) : that.theaterLatitude == null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = theaterId != null ? theaterId.hashCode() : 0;
+        result = 31 * result + (theaterName != null ? theaterName.hashCode() : 0);
+        result = 31 * result + (theaterAddress != null ? theaterAddress.hashCode() : 0);
+        result = 31 * result + (theaterPhone != null ? theaterPhone.hashCode() : 0);
+        result = 31 * result + (theaterLongitude != null ? theaterLongitude.hashCode() : 0);
+        result = 31 * result + (theaterLatitude != null ? theaterLatitude.hashCode() : 0);
+        return result;
+    }
+
+    public Integer getTheaterId() {
+        return theaterId;
+    }
+
+    public void setTheaterId(Integer theaterId) {
+        this.theaterId = theaterId;
+    }
+
+    public String getTheaterName() {
+        return theaterName;
+    }
+
+    public void setTheaterName(String theaterName) {
+        this.theaterName = theaterName;
+    }
+
+    public String getTheaterAddress() {
+        return theaterAddress;
+    }
+
+    public void setTheaterAddress(String theaterAddress) {
+        this.theaterAddress = theaterAddress;
+    }
+
+    public String getTheaterPhone() {
+        return theaterPhone;
+    }
+
+    public void setTheaterPhone(String theaterPhone) {
+        this.theaterPhone = theaterPhone;
+    }
+
+    public String getTheaterLongitude() {
+        return theaterLongitude;
+    }
+
+    public void setTheaterLongitude(String theaterLongitude) {
+        this.theaterLongitude = theaterLongitude;
+    }
+
+    public String getTheaterLatitude() {
+        return theaterLatitude;
+    }
+
+    public void setTheaterLatitude(String theaterLatitude) {
+        this.theaterLatitude = theaterLatitude;
+    }
 }
